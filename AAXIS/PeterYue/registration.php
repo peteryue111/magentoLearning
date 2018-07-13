0b303d6c2f1705fd2f4be1c21613ba76d2467e36
peteryue <peteryue@aaxischina.com>
2018-07-13 10:42:29 +0800

    Add files via upload
    new module

---
+++ AAXIS/PeterYue/registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Copyright © 2016 Magento. All rights reserved. * See COPYING.txt for license details.
+*/
+\Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::MODULE, 'AAXIS_PeterYue',
+__DIR__
+);
